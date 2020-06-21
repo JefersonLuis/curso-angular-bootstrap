cb927e6fbd8331d49d0d53c3f6c746031b98a314
Jeferson Passos <silvajefersonluis@hotmail.com>
2020-06-20 23:10:26 -0300

    Criado a class Curso. Curso-lista.component tratado para mostrar a listagem dinanicamente

--- src/app/curso/curso-lista.component.ts
+++ src/app/curso/curso-lista.component.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Curso } from './curso';
 
 @Component({
   selector: 'app-curso-lista',
-  template: '<h2>Curso Listagem</h2>'
+  templateUrl: './curso-lista.component.html'
 } )
-export class CursoListaComponent {
+export class CursoListaComponent implements OnInit {
+
+  cursos: Curso[] = [] ;
+
+  ngOnInit(): void {
+
+    this.cursos = [
+      {
+        id: 1,
+        nome: 'Angular Forms',
+        imagemUrl: '',
+        preco: 99.99,
+        codigo: 'XPS-8596',
+        duracao: 120,
+        avaliacao: 4.4,
+        lancamentoData: 'Novembro, 17, 2020'
+      }
+      ,
+      {
+        id: 2,
+        nome: 'Angular HTTP',
+        imagemUrl: '',
+        preco: 45.99,
+        codigo: 'XPS-8596',
+        duracao: 120,
+        avaliacao: 4.4,
+        lancamentoData: 'Novembro, 30, 2020'
+      }
+
+    ]
+
+  }
 
 }
