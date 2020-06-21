9a9e101f88ef5108ca2de6b0ac23aaeeb1b0dda2
Jeferson Passos <silvajefersonluis@hotmail.com>
2020-06-21 12:16:20 -0300

    Alterado CursoListaComponent para filtar pelo nome do curso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
+
 import { AppComponent } from './app.component';
 import { CursoListaComponent } from './curso/curso-lista.component';
 import { StarComponent } from './star/star.component';
@@ -13,7 +16,8 @@ import { StarComponent } from './star/star.component';
     StarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/curso/curso-lista.component.ts
+++ src/app/curso/curso-lista.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { Curso } from './curso';
 import { CursoService } from './curso.service';
 
@@ -8,13 +10,26 @@ import { CursoService } from './curso.service';
 } )
 export class CursoListaComponent implements OnInit {
 
+  _filtrarPor: string;
+  _filtradoCursos: Curso[] = [];
+  _cursos: Curso[] = [];
+
   cursos: Curso[] = [] ;
 
   constructor(private cursoService: CursoService){ }
 
   ngOnInit(): void {
+    this._cursos = this.cursoService.buscaCursos();
+    this._filtradoCursos = this._cursos;
 
-    this.cursos = this.cursoService.buscaCursos();
+  }
 
+  set filtro(value: string) {
+    this._filtrarPor = value;
+    this._filtradoCursos = this._cursos.filter((curso: Curso) => curso.nome.toLocaleLowerCase().indexOf(this._filtrarPor.toLocaleLowerCase()) > -1 );
+  }
+
+  get filtro() {
+    return this._filtrarPor ;
   }
 }
