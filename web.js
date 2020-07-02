948c742a7194799e298356dc6909227f86fee3e9
Jeferson Passos <silvajefersonluis@hotmail.com>
2020-07-02 01:20:49 -0300

    correcao - config package.json heruko

--- web.js
+++ web.js
@@ -1,4 +1,4 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/src/')); //aqui você define onde está o index.html da sua aplicação.
-app.listen(process.env.PORT || 3000);
+app.use(express.static(__dirname + '/src/index.html')); //aqui você define onde está o index.html da sua aplicação.
+app.listen(process.env.PORT || 3000 , () => console.log("Server is running..."));
